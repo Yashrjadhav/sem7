4513603ecf36960c30a14d9e92fff64d6f22b464
Yash Jadhav <100465119+Yashrjadhav@users.noreply.github.com>
2024-10-13 16:09:11 +0530

    Update fractionalknap.py

--- fractionalknap.py
+++ fractionalknap.py
@@ -1,25 +1,39 @@
-import collections
-
-Item = collections.namedtuple('Item', ['profit', 'weight'])
+class Item:
+    def __init__(self, profit, weight):
+        self.profit = profit
+        self.weight = weight
 
 def FractionalKnapsack(arr, W):
-    arr.sort(key=lambda x: (x.profit/x.weight), reverse=True)
-    # print(arr)
+    # Sort items based on profit/weight ratio in descending order
+    arr.sort(key=lambda x: (x.profit / x.weight), reverse=True)
+    
     ans = 0.0
-    for i in arr:
-        if(i.weight <= W):
-            W -= i.weight
-            ans += i.profit
+    for item in arr:
+        if item.weight <= W:
+            W -= item.weight
+            ans += item.profit 
         else:
-            ans += i.profit * W // i.weight
+            ans += item.profit * (W / item.weight)  # Use float division here
             break
+            
     return ans
 
-
 def main():
-    # # characters for huffman tree & # frequency of characters
-    arr = [Item(20, 15), Item(40, 40), Item(50, 70)]
-    print(FractionalKnapsack(arr, 50))
+    n = int(input("Enter the number of items: "))  # Get the number of items
+    arr = []
 
+    for i in range(n):
+        profit = float(input(f"Enter profit for item {i + 1}: "))  # Get profit for each item
+        weight = float(input(f"Enter weight for item {i + 1}: "))  # Get weight for each item
+        arr.append(Item(profit, weight))
+
+    W = float(input("Enter the capacity of the knapsack: "))  # Get the capacity of the knapsack
+    max_profit = FractionalKnapsack(arr, W)
+    print(f"The maximum profit is: {max_profit}")
 
 main()
+
+# def main():
+#     arr = [Item(20, 15), Item(40, 40), Item(50, 70)]
+# #     print(FractionalKnapsack(arr, 50))
+# ans=55
